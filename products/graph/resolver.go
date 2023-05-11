bdbf1052795cfc1d616a54e4de1bcc671082a8fe
Marcelo Aguero <marceloaguero@gmail.com>
2023-05-10 21:09:36 -0300

    modify server and add docker-compose

--- products/graph/resolver.go
+++ products/graph/resolver.go
@@ -1,7 +1,17 @@
 package graph
 
+import "github.com/marceloaguero/go-graphql-products/products/model/product"
+
 // This file will not be regenerated automatically.
 //
 // It serves as dependency injection for your app, add any dependencies you require here.
 
-type Resolver struct{}
+type Resolver struct {
+	usecase product.Usecase
+}
+
+func NewResolver(uc product.Usecase) *Resolver {
+	return &Resolver{
+		usecase: uc,
+	}
+}
