9b843ddd00de038d7f327479e1a15b2e6905f867
Marcelo Aguero <marceloaguero@gmail.com>
2023-05-13 20:49:12 -0300

    change server

--- providers/graph/resolver.go
+++ providers/graph/resolver.go
@@ -1,7 +1,17 @@
 package graph
 
+import "github.com/marceloaguero/go-graphql-products/providers/model/provider"
+
 // This file will not be regenerated automatically.
 //
 // It serves as dependency injection for your app, add any dependencies you require here.
 
-type Resolver struct{}
+type Resolver struct {
+	usecase provider.Usecase
+}
+
+func NewResolver(uc provider.Usecase) *Resolver {
+	return &Resolver{
+		usecase: uc,
+	}
+}
--- providers/server.go
+++ providers/server.go
@@ -8,6 +8,8 @@ import (
 	"github.com/99designs/gqlgen/graphql/handler"
 	"github.com/99designs/gqlgen/graphql/playground"
 	"github.com/marceloaguero/go-graphql-products/providers/graph"
+	"github.com/marceloaguero/go-graphql-products/providers/model/provider"
+	repo "github.com/marceloaguero/go-graphql-products/providers/repository"
 )
 
 const defaultPort = "8080"
@@ -18,7 +20,19 @@ func main() {
 		port = defaultPort
 	}
 
-	srv := handler.NewDefaultServer(graph.NewExecutableSchema(graph.Config{Resolvers: &graph.Resolver{}}))
+	dbDsn := os.Getenv("DB_DSN")
+	dbName := os.Getenv("DB_NAME")
+
+	repository, err := repo.NewRepo(dbDsn, dbName)
+	if err != nil {
+		log.Panic(err)
+	}
+
+	usecase := provider.NewUsecase(repository)
+	resolver := graph.NewResolver(usecase)
+
+	//srv := handler.NewDefaultServer(graph.NewExecutableSchema(graph.Config{Resolvers: &graph.Resolver{}}))
+	srv := handler.NewDefaultServer(graph.NewExecutableSchema(graph.Config{Resolvers: resolver}))
 
 	http.Handle("/", playground.Handler("GraphQL playground", "/query"))
 	http.Handle("/query", srv)
