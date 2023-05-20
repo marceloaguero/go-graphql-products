6d41aae80952b57b9c1914bc0176203c5db0fdcb
Marcelo Aguero <marceloaguero@gmail.com>
2023-05-20 20:48:58 -0300

    add go generate

--- products/graph/resolver.go
+++ products/graph/resolver.go
@@ -1,5 +1,7 @@
 package graph
 
+//go:generate go run github.com/99designs/gqlgen
+
 import "github.com/marceloaguero/go-graphql-products/products/model/product"
 
 // This file will not be regenerated automatically.
